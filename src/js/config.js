50232bcf24d0a0cdc71735ee6bdd3c92ab3e77e5
emmanuel <emmanuelvostu@gmail.com>
2024-11-03 23:51:10 +0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const RES_PER_PAGE = 10;
+export const TIMEOUT_SEC = 10;
+export const KEY = 'f033f95b-f7ba-4955-9d7e-bb45576dcb04';
+export const MODAL_CLOSE_SEC = 2.5;
